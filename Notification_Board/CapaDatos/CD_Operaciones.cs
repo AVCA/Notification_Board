07ec4b5f67e8ad06e5a35936a710a50bff3ae20d
Ana Victoria Cavazos Argot <anavca@outlook.com>
2018-05-04 02:03:50 -0500

    Incluye el trabajo de Alan y Andres

--- Notification_Board/CapaDatos/CD_Operaciones.cs
+++ Notification_Board/CapaDatos/CD_Operaciones.cs
@@ -31,6 +31,9 @@ namespace CapaDatos
                 case "Horarios":
                     comando.CommandText = "call VerImparte";
                     break;
+                case "Archivo":
+                    comando.CommandText = "call VerArchivos";
+                    break;
             }
             
             leer = comando.ExecuteReader();
@@ -38,8 +41,6 @@ namespace CapaDatos
             conexion.CerrarConexion();
             return tabla;
         }
-
-        // select InsertarProfesor(02,'Maestro 2');
 
         public String Operaciones(string tabla,string operacion, string v1, string v2, string v3, string v4, string v5)
         {
@@ -85,6 +86,20 @@ namespace CapaDatos
                             break;
                         case "Update":
                             comando.CommandText = "select ActualizarMateria('" + v1 + "','" + v2 + "','" + v3 + "') as resp";
+                            break;
+                    }
+                    break;
+                case "Archivo":
+                    switch (operacion)
+                    {
+                        case "Insert":
+                            comando.CommandText = "select InsertarArchivo('" + v1 + "','" + v2 + "','" + v3 + "')";
+                            break;
+                        case "Delete":
+                            comando.CommandText = "call EliminarMateria('" + v1 + "')";
+                            break;
+                        case "Update":
+                            comando.CommandText = "select ActualizarMateria('" + v1 + "','" + v2 + "','" + v3 + "')";
                             break;
                     }
                     break;
